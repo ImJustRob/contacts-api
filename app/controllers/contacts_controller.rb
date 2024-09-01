0ace4ebf10e391fb5d1d8ab748c744e335fd935f
ImJustRob <robertalothian@hotmail.com>
2024-09-01 13:53:45 -0400

    add json builder

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,11 @@
 class ContactsController < ApplicationController
+    def all
+        @contacts = Contact.all
+        render template: "contacts/index"
+      end
+   
     def first_contact
-        contact = Contact.find_by(id: 1)
-        p contact
-        render json: {
-            first_name: contact.first_name,
-            last_name: contact.last_name,
-            email: contact.email,
-            phone_number: contact.phone_number
-        }
+        @contact = Contact.find_by(id: 1)
+        render template: "contacts/show"
     end
 end
